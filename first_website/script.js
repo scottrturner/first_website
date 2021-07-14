5a20540adf1440e7857c7b14513132375ca8e5a8
Scott Turner <scottrogersturner@gmail.com>
2021-07-14 11:28:35 -0500

    added readme

--- first_website/script.js
+++ first_website/script.js
@@ -14,11 +14,14 @@ myImage.onclick = function() {
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
-
 function setUserName() {
   let myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  myHeading.textContent = 'Mozilla is cool, ' + myName;
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
 }
 
 if(!localStorage.getItem('name')) {
